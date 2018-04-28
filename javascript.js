d8f8d2dd86a4df25b2a83bede560c0aac6aefdd8
MichaelSParkin3 <MichaelSParkin3@gmail.com>
2018-04-28 00:33:45 -0700

    Added semi functionality in js

--- javascript.js
+++ javascript.js
@@ -1,15 +1,46 @@
 // A $( document ).ready() block.
-$( document ).ready(function() {
-
+$( document ).ready(function start () {
 
   $("button").click(function() {
 
     var id = this.id;
+    var text = $(".screen-numbers").text();
+    var textEquation = $(".screen-equation").text();
+    var hasDecimal = text.includes(".");
+    //if the current number already has a decimal and its trying to add another then only console.log and nothing else
+    if (hasDecimal && this.id == ".") {
+      console.log("already has decimal");
+//if the button press is a number
+}else if ((!isNaN(id)) || (this.id == ".")) {
+      //Save current numbers on screen
+      var text = $(".screen-numbers").text();
+      var textEquation = $(".screen-equation").text();
+      //If the first char is a 0 then dont repaste the saved text
+      if (text.charAt(0)=="0" && textEquation.charAt(0)=="0"){
+        $(".screen-numbers").text(this.id);
+        $(".screen-equation").text(this.id);
 
-    if (!isNaN(id)) {
-      if (id == AC) {
+      }else if(text.charAt(0)=="0"){
+        $(".screen-numbers").text(this.id);
+        $(".screen-equation").text(textEquation+this.id);
+      }else{
+        //repaste the text with the new text
+      $(".screen-numbers").text(text+this.id);
+      $(".screen-equation").text(textEquation+this.id);
+    }
+//if the button pressed is not a number.
+    } else if (isNaN(id)) {
+
+      if (id == "AC") {
         $(".screen-numbers").text("0");
+        $(".screen-equation").text("0");
       }
+
+      if (id == "/" || id == "x" || id == "-" || id == "+") {
+        $(".screen-numbers").text("0");
+        $(".screen-equation").text(textEquation+this.id);
+      }
+
     }
 
 });
