89c4079521ee21a029a3e43100d010afa5039f2a
MichaelSParkin3 <MichaelSParkin3@gmail.com>
2018-04-29 18:37:49 -0700

    Fixed some bugs reported from the fcc forums

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 // A $( document ).ready() block.
 $( document ).ready(function start () {
-  console.log("hi".charAt(-1));
-console.log(eval('2 + 2'));
-  console.log(eval('((2 + 5) / (4 * 3))+2'));
+
+
+  console.log(("(50/5)*10*(5+5)*1".split("*").length - 1));
+
+
   var flipped = false;
 
   $("button").click(function() {
@@ -21,8 +23,12 @@ console.log(eval('2 + 2'));
     }else {
       $(this).addClass( "click" );
     }
+    //If the only button in the equation is a 0 and a button new buton is pressed then just remove the zero.
+    if (textEquation.length == 1 && textEquation.charAt(0) == "0") {
+      $(".screen-equation").text("");
+    }
 
-
+    //If length of equation or number part of screen is too long then display error directions
     if (text.replace(/\D/g,'').length >= 11 || textEquation.replace(/\D/g,'').length >= 20) {
       $(".screen-numbers").text("# TOO LONG");
       $(".screen-equation").text('PRESS "AC"');
@@ -42,7 +48,7 @@ console.log(eval('2 + 2'));
       //If the first char is a 0 then dont repaste the saved text
       if (text.charAt(0)=="0" && textEquation.charAt(0)=="0"){
         $(".screen-numbers").text(this.id);
-        $(".screen-equation").text(this.id);
+        $(".screen-equation").text(textEquation+this.id);
 
       }else if(text.charAt(0)=="0"){
         $(".screen-numbers").text(this.id);
@@ -55,18 +61,24 @@ console.log(eval('2 + 2'));
 //if the button pressed is not a number.
     } else if (isNaN(id)) {
 
-
+      //If smile button is pressed then add animation class and funny numbers
       if (id == "smile") {
-        flipped = true;
+
         if ($( ".calculator" ).hasClass( "rotate-back" )) {
           $( ".calculator" ).removeClass( "rotate-back" );
         }
         $( ".calculator" ).addClass( "rotate" );
         $(".screen-numbers").text("58008");
         $(".screen-equation").text('58008');
+        if (flipped == true) {
+          $( ".calculator" ).removeClass( "rotate" );
+          $( ".calculator" ).addClass( "rotate-back" );
+          flipped = false;
+        }
+        flipped = true;
       }
 
-
+      //resets calculator
       if (id == "AC") {
         if (flipped == true) {
           $( ".calculator" ).removeClass( "rotate" );
@@ -76,7 +88,7 @@ console.log(eval('2 + 2'));
         $(".screen-numbers").text("0");
         $(".screen-equation").text("0");
       }
-
+      //runs when math operator is pressed
       if (id == "/" || id == "x" || id == "-" || id == "+") {
         console.log(text);
         if (text.charAt(0) == "=") {
@@ -91,10 +103,11 @@ console.log(eval('2 + 2'));
 
       if (id == "=") {
         finalEquation = $(".screen-equation").text();
-        finalEquation = finalEquation.replace("x","*");
+        finalEquation = finalEquation.replace(/x/g, "*");
         $(".screen-equation").text(textEquation+"=");
         try {
-        $(".screen-numbers").text("="+eval(finalEquation));
+          var finalAnswer = eval(finalEquation);
+          console.log(finalAnswer.toFixed(5));
       }
       catch(error) {
         console.error(error);
@@ -103,6 +116,15 @@ console.log(eval('2 + 2'));
         // expected output: SyntaxError: unterminated string literal
         // Note - error messages will vary depending on browser
       }
+      var splitAnswer = finalAnswer.toFixed(5).split(".");
+      console.log(splitAnswer);
+      console.log(/^(.)\1+$/.test(splitAnswer));
+      if (/^(.)\1+$/.test(splitAnswer[1])) {
+        $(".screen-numbers").text("="+splitAnswer[0]);
+      } else {
+        $(".screen-numbers").text("="+finalAnswer.toFixed(5));
+      }
+
       }
 
     }
